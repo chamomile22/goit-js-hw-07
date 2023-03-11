ae13344870da64994ec7063a5eeaf3d82de2d698
Yana Babak <yana.i.babak@gmail.com>
2023-03-11 22:03:09 +0200

    task 1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// ---Creating and rendering of gallery items
+const gallery = document.querySelector(".gallery");
+const createdItems = galleryItems
+	.map(
+		({ preview, original, description }) => `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+	)
+	.join("");
+
+gallery.insertAdjacentHTML("afterbegin", createdItems);
+
+// ---Delegation
+
+const handleGalleryContainerClick = (event) => {
+	event.preventDefault();
+	if (!event.target.classList.contains("gallery__image")) {
+		return;
+	}
+
+	const instance = basicLightbox.create(
+		`
+    <img src="${event.target.dataset.source}">
+`
+	);
+	instance.show();
+
+	if (instance.visible()) {
+		event.currentTarget.addEventListener("keydown", (event) => {
+			if (event.key === "Escape") {
+				instance.close();
+			}
+		});
+	}
+};
+
+gallery.addEventListener("click", handleGalleryContainerClick);
