1187feffdee362b74f357614fdd07c8ece8307a0
Yana Babak <yana.i.babak@gmail.com>
2023-03-11 22:37:40 +0200

    task 2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// ---Creating and rendering of gallery items
+const gallery = document.querySelector(".gallery");
+const createdItems = galleryItems
+	.map(
+		({ preview, original, description }) => `
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+	)
+	.join("");
+
+gallery.insertAdjacentHTML("afterbegin", createdItems);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+	captionsData: "alt",
+	captionDelay: 250,
+});
+const handleGalleryContainerClick = (event) => {
+	event.preventDefault();
+	if (!event.target.classList.contains("gallery__image")) {
+		return;
+	}
+};
+
+gallery.addEventListener("click", handleGalleryContainerClick);
